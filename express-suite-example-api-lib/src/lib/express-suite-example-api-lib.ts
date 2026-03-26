d115580895657f2329579790471844aebb96931e
Jessica Mulein <jessica@digitaldefiance.org>
2026-03-26 13:49:44 -0700

    Initial commit

---
+++ express-suite-example-api-lib/src/lib/express-suite-example-api-lib.ts
@@ -0,0 +1,3 @@
+export function expressSuiteExampleApiLib(): string {
+  return 'express-suite-example-api-lib';
+}
---
+++ express-suite-example-react/src/app/menus/extraMenu.tsx
@@ -0,0 +1,34 @@
+import {
+  createMenuType,
+  IMenuConfig,
+  MenuTypes,
+} from '@digitaldefiance/express-suite-react-components';
+import { Dashboard, Email } from '@mui/icons-material';
+
+const ExtraMenu = createMenuType('ExtraMenu');
+
+export const extraMenuConfig: IMenuConfig = {
+  menuType: ExtraMenu,
+  menuIcon: <Dashboard />,
+  priority: 1,
+  options: [
+    {
+      id: 'extra-option-1',
+      label: 'Extra Option 1',
+      link: '/extra-1',
+      requiresAuth: true,
+      includeOnMenus: [ExtraMenu],
+      index: 0,
+    },
+    {
+      id: 'admin-emails',
+      label: 'Admin Emails',
+      icon: <Email />,
+      link: '/admin/emails',
+      requiresAuth: true,
+      requiresAdmin: true,
+      includeOnMenus: [ExtraMenu, MenuTypes.SideMenu],
+      index: 1,
+    },
+  ],
+};
